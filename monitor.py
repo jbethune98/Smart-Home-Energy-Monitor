598378aec539289d80be5cf05b688fb4a94909da
jbethune98 <jbethune98@gmail.com>
2026-06-27 01:49:46 -0400

    erase noise monitor.py
    code was edited so that the noise when there is no load is ignored and reads no current

--- monitor.py
+++ monitor.py
@@ -8,7 +8,7 @@ from adafruit_ads1x15.analog_in import AnalogIn
 BURDEN_RESISTOR = 50.0       # ohms
 CT_RATIO = 2000.0            # SCT-013-000: 100A / 0.05A = 2000
 VOLTAGE_RMS = 120.0          # wall voltage estimate
-
+noise_floor = .05            # no load current noise 
 i2c = busio.I2C(board.SCL, board.SDA)
 ads = ADS.ADS1115(i2c)
 chan = AnalogIn(ads, 0)
@@ -27,7 +27,8 @@ def read_current(samples=1000):
 
     ct_secondary_current = voltage_rms / BURDEN_RESISTOR
     primary_current = ct_secondary_current * CT_RATIO
-
+    if primary_current < noise_floor            #if there is no load
+        primary_current = 0                     #sets no load current to zero to ignore noise.
     return avg, voltage_rms, primary_current
 
 while True:
